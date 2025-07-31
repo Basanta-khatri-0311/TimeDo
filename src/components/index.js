9b4f3cbdc2920e6e6646c60ad1cc5d71ba1eefd7
Basanta-khatri-0311 <khatribasanta.git@gmail.com>
2025-07-31 14:47:38 +0545

    seperate file for every components import for clean look

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export { AddTask } from "./AddTask";
+export { CalendarComponent } from "./CalendarComponent";
+export { Header } from "./Header";
+export { CompleteStatus } from "./CompleteStatus";
+export { TaskList } from "./TaskList";
+export { StartButton, PauseButton, EndButton } from "./Button";
