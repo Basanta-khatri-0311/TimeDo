6d8b0fe4fdcc3de048c9c897f03e6429216713dd
Basanta-khatri-0311 <khatribasanta.git@gmail.com>
2025-07-31 14:47:00 +0545

    named import implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import Home from "./pages/Home";
+import {Home} from "./pages/Home";
 
-const App = () => {
+export const App = () => {
   return (
     <>
       <Home />
@@ -9,4 +9,3 @@ const App = () => {
   );
 };
 
-export default App;
